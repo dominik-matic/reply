1e9e72460c36b8e1199ea031e7a7dd200d217de6
Dominik <dominik.matic@outlook.com>
2020-03-12 18:08:05 +0100

    a

--- src/funcs.cpp
+++ src/funcs.cpp
@@ -2,7 +2,7 @@
 #define __STRUCTS_HPP
 #include "structs.hpp"
 
-template typename<T, K>
+typename<T, K>
 unsigned int calcTotalPotential(T r1, K r2) {
     unsigned int bonusPotential = 0;
     unsigned int workPotential = 0;
@@ -11,6 +11,27 @@ unsigned int calcTotalPotential(T r1, K r2) {
     if(r1.company == r2.company) {
         bonusPotential = r1.bonus * r2.bonus;
     }
+
+
+    //work potential
+    if(Developer d1 = dynamic_cast<Developer>(r1)
+        && Developer d2 = dynamic_cast<Developer>(r2)) {
+        unsigned int common = 0;
+        set<string> disjunct;
+        disjunct.insert(d1.skills.begin(), d1.skills.end());
+        disjunct.insert(d2.skills.begin(), d2.skills.end());
+        for(string s1 : d1.skills) {
+            for(string s2 : d2.skills) {
+                if(s1 == s2) {
+                    disjunct.erase(s1);
+                    ++common;
+                    break;
+                }
+            }
+        }
+    }
+
+    return bonusPotential + workPotential;
 }
 
 
